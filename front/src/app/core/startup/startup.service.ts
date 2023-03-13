552009af85d1bac48a7dc2cf91b0f2c43184a849
Alfredo <alfredo@orofino.org>
2023-03-13 20:16:08 -0300

    Fix prettier erros

--- front/src/app/core/startup/startup.service.ts
+++ front/src/app/core/startup/startup.service.ts
@@ -33,105 +33,9 @@ export class StartupService {
     iconSrv.addIcon(...ICONS_AUTO, ...ICONS);
   }
 
-  private viaHttp(): Observable<void> {
-    return this.httpClient.get('assets/menu2.json').pipe(
-      //    return this.httpClient.get('assets/tmp/app-data.json').pipe(
-      catchError((res: NzSafeAny) => {
-        console.warn(`StartupService.load: Network request failed`, res);
-        return of({});
-      }),
-      mergeMap((res: NzSafeAny) => {
-        // Application information: including site name, description, year
-        this.settingService.setApp(res.app);
-        // User information: including name, avatar, email address
-
-        try {
-          const tkndec: any = this.tokenService.get(JWTTokenModel);
-          if (tkndec) {
-            this.settingService.setUser({ name: tkndec.payload.name, email: tkndec.payload.email, key: tkndec.payload.username });
-          }
-        } catch (error) {}
-
-        // ACL: Set the permissions to full, https://ng-alain.com/acl/getting-started
-        this.aclService.setFull(true);
-        // Menu data, https://ng-alain.com/theme/menu
-        //this.menuService.add(res.menu);
-        // Can be set page suffix title, https://ng-alain.com/theme/title
-        this.titleService.suffix = res.app.name;
-
-        return this.httpClient.get('assets/menu.json').pipe(
-          catchError((res: NzSafeAny) => {
-            console.warn(`StartupService.load: Menu request failed`, res);
-            return of({});
-          }),
-          map((res: NzSafeAny) => {
-            this.menuService.add(res.menu);
-          })
-        );
-      })
-      /*
-      map((res: NzSafeAny) => {
-        // Application information: including site name, description, year
-        this.settingService.setApp(res.app);
-        // User information: including name, avatar, email address
-        this.settingService.setUser(res.user);
-        // ACL: Set the permissions to full, https://ng-alain.com/acl/getting-started
-        this.aclService.setFull(true);
-        // Menu data, https://ng-alain.com/theme/menu
-        //this.menuService.add(res.menu);
-        // Can be set page suffix title, https://ng-alain.com/theme/title
-        this.titleService.suffix = res.app.name;
-      })*/
-    );
-  }
-
-  private viaMock(): Observable<void> {
-    // const tokenData = this.tokenService.get();
-    // if (!tokenData.token) {
-    //   this.injector.get(Router).navigateByUrl('/passport/login');
-    //   return;
-    // }
-    // mock
-    const app: any = {
-      name: `FABI`,
-      description: `Gestión COFyBCF`
-    };
-    const user: any = {
-      name: 'Admin',
-      avatar: './assets/tmp/img/avatar.jpg',
-      email: 'cipchk@qq.com',
-      token: '123456789'
-    };
-    // Application information: including site name, description, year
-    this.settingService.setApp(app);
-    // User information: including name, avatar, email address
-    this.settingService.setUser(user);
-    // ACL: Set the permissions to full, https://ng-alain.com/acl/getting-started
-    this.aclService.setFull(true);
-    // Menu data, https://ng-alain.com/theme/menu
-    this.menuService.add([
-      {
-        text: 'Principal',
-        group: true,
-        children: [
-          {
-            text: 'Escritorio',
-            link: '/dashboard',
-            icon: { type: 'icon', value: 'appstore' }
-          }
-        ]
-      }
-    ]);
-    // Can be set page suffix title, https://ng-alain.com/theme/title
-    this.titleService.suffix = app.name;
-
-    return of();
-  }
-
   load(): Observable<void> {
-    return this.viaHttp();
     const defaultLang = this.i18n.defaultLang;
-    return zip(this.i18n.loadLangData(defaultLang), this.httpClient.get('assets/tmp/app-data.json')).pipe(
+    return zip(this.i18n.loadLangData(defaultLang), this.httpClient.get('assets/app-data.json')).pipe(
       // 接收其他拦截器后产生的异常消息
       catchError(res => {
         console.warn(`StartupService.load: Network request failed`, res);
@@ -153,6 +57,13 @@ export class StartupService {
         // 设置页面标题的后缀
         this.titleService.default = '';
         this.titleService.suffix = appData.app.name;
+
+        try {
+          const tkndec: any = this.tokenService.get(JWTTokenModel);
+          if (tkndec) {
+            this.settingService.setUser({ name: tkndec.payload.name, email: tkndec.payload.email, key: tkndec.payload.username });
+          }
+        } catch (error) {}
       })
     );
   }
