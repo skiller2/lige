3543a718dc5357240f594067701fe7ebcbe5ae76
Alfredo <alfredo@orofino.org>
2023-04-07 01:54:21 -0300

    PDF Conversion

--- front/src/app/shared/pdfviewer/pdfviewer.component.ts
+++ front/src/app/shared/pdfviewer/pdfviewer.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { CreateOptions, PDFDocument, PDFImage, PDFPage } from 'pdf-lib';
+import { CreateOptions, PDFDocument, PDFImage, PDFPage, PageSizes } from 'pdf-lib';
 import { DownloadService } from 'src/app/services/download.service';
 import { grayscale, rgb } from 'pdf-lib'
 import { BehaviorSubject } from 'rxjs';
@@ -12,7 +12,7 @@ import { BehaviorSubject } from 'rxjs';
 export class PdfviewerComponent implements OnChanges {
 
   @Input() bufferPDF: Uint8Array = new Uint8Array()
-  @Input() namePDF: string = 'file'
+  @Input() namePDF: string = 'file.pdf'
 
   $currentDownloadablePDF = new BehaviorSubject<Uint8Array>(new Uint8Array())
   $isProcessing = new BehaviorSubject(false)
@@ -21,11 +21,14 @@ export class PdfviewerComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     this.createPDF(this.bufferPDF)
+    var ext = this.namePDF.substring(this.namePDF.lastIndexOf('.') + 1)
+    var name = this.namePDF.substring(0,this.namePDF.lastIndexOf('.'))
+    this.namePDF = `${name}_FIRMADO.${ext}`    
   }
 
   async createPDF(bufferPDF: Uint8Array) {
     if (bufferPDF.length == 0) return
-    this.$currentDownloadablePDF.next(new Uint8Array())
+    this.$currentDownloadablePDF.next(new Uint8Array())  //Vacío el contenido actual
     this.$isProcessing.next(true)
 
     const originPDF = await PDFDocument.load(bufferPDF)
@@ -37,35 +40,19 @@ export class PdfviewerComponent implements OnChanges {
     const embededPages = await newPdf.embedPages(originPDFPages)
     const image = await fetch('assets/pdf/signature.png').then(res => res.arrayBuffer())
     const embededImage = await newPdf.embedPng(image)
+    const scaleImage = embededImage.scale(1/60)
 
-
-    // Array(Math.ceil(embededPages.length / 2)).fill(0).forEach((_, index) => {
-    //   const page = newPdf.addPage()
-    //   page.setSize(595.28, 841.89)
-
-    //   const pageRatio = page.getWidth() / page.getHeight()
-    //   Array(2).fill(0).forEach((_, idx)  => {
-    //     const embPage = embededPages[index + idx]
-    //     embPage.scale(1/pageRatio)
-
-    //     page.drawPage(embPage, {x: (page.getWidth() - embPage.width) / 2, y: page.getHeight() / -2 * (idx - 1)})
-    //     page.drawImage(embededImage, {x: 210, y: 80 + embPage.height * (idx), width: 60, height: 60 *  embededImage.height / embededImage.width})
-    //   });
-
-    // })
     let currentPage: PDFPage;
 
     embededPages.forEach((embPage, index) => {
       if (index % 2 == 0) {
-        currentPage = newPdf.addPage()
-        currentPage.setSize(595.28, 841.89)
+        currentPage = newPdf.addPage(PageSizes.A4)
       }
       const pageRatio = currentPage.getWidth() / currentPage.getHeight()
 
-      embPage.scale(1 / pageRatio)
-      // console.log(index, currentPage.getHeight() / 2 * ((index) % 2))
+//      embPage.scale(1 / pageRatio)
       currentPage.drawPage(embPage, { x: (currentPage.getWidth() - embPage.width) / 2, y: currentPage.getHeight() / 2 * ((index+1) % 2) })
-      currentPage.drawImage(embededImage, { x: 210, y: 80 + embPage.height * ((index+1) % 2), width: 60, height: 60 * embededImage.height / embededImage.width })
+      currentPage.drawImage(embededImage, { x: 210, y: 80 + embPage.height * ((index) % 2), width:scaleImage.width, height:scaleImage.height })
     })
 
     const arrayBuffer = await newPdf.save()
@@ -74,33 +61,11 @@ export class PdfviewerComponent implements OnChanges {
     this.$currentDownloadablePDF.next(arrayBuffer)
   }
 
-  // async createPDF(pdf?: PDFDocument) {
-  //   const newPdf = await PDFDocument.create()
-
-  //   const sampleBytes = await fetch('assets/pdf/sample.pdf').then(res => res.arrayBuffer())
-  //   const samplePDF = await PDFDocument.load(sampleBytes)
-
-  //   const samplePDFPages = samplePDF.getPages()
-
-  //   const embededPages = await newPdf.embedPages(samplePDFPages)
-
-  //   const page = newPdf.addPage()
-
-  //   page.drawPage(embededPages[0], {
-  //     x: 0,
-  //     width: page.getWidth() / 2,
-  //   })
-  //   page.drawPage(embededPages[0], {
-  //     x: page.getWidth() / 2,
-  //     width: page.getWidth() / 2,
-  //   })
-  //   const arrayBuffer = await newPdf.save()
-  //   this.$currentDownloadablePDF.next(arrayBuffer)
-  // }
-
   async download() {
     if (this.$currentDownloadablePDF.value) {
-      this.downloadService.downloadBlob(this.$currentDownloadablePDF.value, `${this.namePDF}_FIRMADO`, 'pdf')
+
+
+      this.downloadService.downloadBlob(this.$currentDownloadablePDF.value, this.namePDF,  'pdf')
     }
   }
 
