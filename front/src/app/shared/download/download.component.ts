92c204495705d347f91d95ef243d78d712d19972
Alfredo Orofino <rorofino10@gmail.com>
2023-04-23 23:24:28 -0300

    Download Component

---
+++ front/src/app/shared/download/download.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { DownloadService } from 'src/app/services/download.service';
+
+@Component({
+  selector: 'app-download',
+  templateUrl: './download.component.html',
+  styleUrls: ['./download.component.less']
+})
+export class DownloadComponent {
+
+  constructor(private downloadService: DownloadService){}
+  @Input() currentDownloadableFile: BlobPart | null = null
+  @Input() name: string = ''
+  @Input() completedText: string = ''
+  @Input() loadingText: string = ''
+
+  async download() {
+    if (this.currentDownloadableFile) {
+      this.downloadService.downloadBlob(this.currentDownloadableFile, `${this.name}`, 'pdf')
+    }
+  }
+
+}
