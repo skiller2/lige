4b2d147a97b3f43f821a791479aace936180be3e
Alfredo Orofino <rorofino10@gmail.com>
2023-04-28 03:34:57 -0300

    Prettier

--- front/.prettierrc.js
+++ front/.prettierrc.js
@@ -1,13 +1,24 @@
+// module.exports = {
+//   singleQuote: true,
+//   useTabs: false,
+//   printWidth: 140,
+//   tabWidth: 2,
+//   semi: true,
+//   htmlWhitespaceSensitivity: 'strict',
+//   arrowParens: 'avoid',
+//   bracketSpacing: true,
+//   proseWrap: 'preserve',
+//   trailingComma: 'none',
+//   endOfLine: 'crlf'
+// };
 module.exports = {
-  singleQuote: true,
-  useTabs: false,
-  printWidth: 140,
   tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
   semi: true,
-  htmlWhitespaceSensitivity: 'strict',
-  arrowParens: 'avoid',
   bracketSpacing: true,
-  proseWrap: 'preserve',
-  trailingComma: 'none',
-  endOfLine: 'crlf'
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  printWidth: 80,
 };
