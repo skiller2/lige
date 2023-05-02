6d06f5a91e6d2e7e02aba82367bd7163188d37fd
Alfredo <alfredo@orofino.org>
2023-05-02 11:55:57 -0300

    Optimized dashboard (using Roman's logic)

--- back/src/routes/init.routes.ts
+++ back/src/routes/init.routes.ts
@@ -5,9 +5,18 @@ import { authMiddleware } from "../middlewares/middleware.module";
 export const initRouter = Router();
 const base = "";
 
-initRouter.get(`${base}/stats`,  authMiddleware.verifyToken, (req, res) => {
-    initController.getStats(req, res);
+initRouter.get(`${base}/stats/horastrabajadas/:anio`,  authMiddleware.verifyToken, (req, res) => {
+    initController.getHorasTrabajadas(req, res);
 })
+
+initRouter.get(`${base}/stats/adelantospendientes`,  authMiddleware.verifyToken, (req, res) => {
+    initController.getAdelantosPendientes(req, res);
+})
+
+initRouter.get(`${base}/stats/excepcionespendientes`,  authMiddleware.verifyToken, (req, res) => {
+    initController.getExcepcionesPendientes(req, res);
+})
+
 
 initRouter.get(`${base}/stats/objetivosactivos`,  authMiddleware.verifyToken, (req, res) => {
     initController.getObjetivosActivos(req, res);
