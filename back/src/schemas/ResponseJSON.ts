d4e2b899ff205cc061d66c6abdd3b0860261bb85
Alfredo Orofino <rorofino10@gmail.com>
2023-05-17 00:21:55 -0300

    PDF Not working

--- back/src/schemas/ResponseJSON.ts
+++ back/src/schemas/ResponseJSON.ts
@@ -1,4 +1,25 @@
 export interface ResponseJSON<T> {
-    msg: string,
-    data: T
+  msg: string;
+  data: T;
+  stamp: Date;
+}
+
+export interface DescuentoJSON {
+  PersonalId: number;
+  CUIT: number;
+  ApellidoNombre: string;
+  PersonalEstado: string;
+  PersonalIdJ: number;
+  CUITJ: number;
+  ApellidoNombreJ: string;
+  monto: null | number;
+  PersonalOtroDescuentoAnoAplica: null | number;
+  PersonalOtroDescuentoMesesAplica: null | number;
+  PersonalOtroDescuentoDescuentoId: null | number;
+  clicked?: boolean;
+}
+
+export interface ResponseDescuentos {
+  RegistrosConComprobantes: number;
+  Registros: DescuentoJSON[];
 }
