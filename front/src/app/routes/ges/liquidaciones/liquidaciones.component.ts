0826133a3c5c61a8828a1ee9b71fd9ed470cceaa
Alfredo <alfredo@orofino.org>
2023-10-04 00:28:06 -0300

    Fix Add Remove empty row

--- front/src/app/routes/ges/liquidaciones/liquidaciones.component.ts
+++ front/src/app/routes/ges/liquidaciones/liquidaciones.component.ts
@@ -397,8 +397,9 @@ export class LiquidacionesComponent {
 
   onCellChanged(e: any) {
     const row = e.detail.args.item
-    if (!row.detalle && !row.des_movimiento || !row.ObjetivoDescripcion || !row.PersonalDescripcion || !row.monto)
-      this.angularGridEdit.gridService.deleteItem(row.id)
+//    console.log('row',row)
+    if (!row.detalle && !row.des_movimiento && !row.ObjetivoDescripcion && !row.PersonalDescripcion && !row.monto)
+      this.angularGridEdit.gridService.deleteItem(row)
 
     console.log('grabar aca cuando estén todos los datos', row,e)
     if (row.detalle && row.des_movimiento && (row.ObjetivoDescripcion || row.PersonalDescripcion) && row.monto) { 
