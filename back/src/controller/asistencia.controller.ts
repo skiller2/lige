81c521071e91298a688f9c100ab6b17ede1284c3
Alfredo <alfredo@orofino.org>
2023-12-20 23:41:17 -0300

    Validate access

--- back/src/controller/asistencia.controller.ts
+++ back/src/controller/asistencia.controller.ts
@@ -16,8 +16,11 @@ export class AsistenciaController extends BaseController {
 
     const queryRunner = dataSource.createQueryRunner();
 
-
     try {
+      if (!await this.hasGroup(req, 'liquidaciones') && !await this.hasGroup(req, 'administrativo') && !await this.hasAuthObjetivo(anio, mes, res, Number(ObjetivoId), queryRunner))
+        throw new ClientException(`No tiene permisos para habilitar carga`)
+
+
       await queryRunner.startTransaction()
       await this.addAsistenciaPeriodo(anio, mes, ObjetivoId, queryRunner)
       await queryRunner.commitTransaction();
@@ -41,7 +44,7 @@ export class AsistenciaController extends BaseController {
     const cabecera = await AsistenciaController.getObjetivoAsistenciaCabecera(anio, mes, ObjetivoId, queryRunner)
     if (cabecera.length == 0)
       throw new ClientException('Objetivo no localizado')
-
+    
     const contratos = await ObjetivoController.getObjetivoContratos(ObjetivoId, anio, mes, queryRunner)
     if (contratos.length == 0)
       throw new ClientException(`No tiene contrato vigente para el período ${anio}/${mes}`)
@@ -128,6 +131,10 @@ export class AsistenciaController extends BaseController {
       if (cabecera.length == 0)
         throw new ClientException('Objetivo no localizado')
 
+      if (!await this.hasGroup(req, 'liquidaciones') && !await this.hasGroup(req, 'administrativo') && !await this.hasAuthObjetivo(anio, mes, res, Number(ObjetivoId), queryRunner))
+        throw new ClientException(`No tiene permisos para habilitar carga`)
+      
+      
       if (cabecera[0].ObjetivoAsistenciaAnoId == null || cabecera[0].ObjetivoAsistenciaAnoMesId == null)
         throw new ClientException('Periodo de carga de asitencia no generado')
 
