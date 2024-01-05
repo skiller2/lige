da3c388245d131eca0d1e0e01266025f13074c13
Alfredo <alfredo@orofino.org>
2024-01-05 15:17:23 -0300

    Fix banco pendientes

--- back/src/liquidaciones/liquidaciones-banco/liquidaciones-banco.controller.ts
+++ back/src/liquidaciones/liquidaciones-banco/liquidaciones-banco.controller.ts
@@ -389,7 +389,7 @@ export class LiquidacionesBancoController extends BaseController {
 
     return dataSource.query(
       `
-      SELECT CONCAT(banco_id,'-',envio_nro,'-',persona_id) id, liq.persona_id,liq.cbu,liq.importe,liq.banco_id,liq.envio_nro,liq.fecha,liq.tipocuenta_id,banc.bancodescripcion, cuit.PersonalCUITCUILCUIT, CONCAT(TRIM(per.PersonalApellido),', ', TRIM(per.PersonalNombre)) AS ApellidoNombre
+      SELECT CONCAT(liq.banco_id,'-',liq.envio_nro,'-',liq.persona_id,'-',liq.tipocuenta_id) id, liq.persona_id,liq.cbu,liq.importe,liq.banco_id,liq.envio_nro,liq.fecha,liq.tipocuenta_id,banc.bancodescripcion, cuit.PersonalCUITCUILCUIT, CONCAT(TRIM(per.PersonalApellido),', ', TRIM(per.PersonalNombre)) AS ApellidoNombre
       FROM lige.dbo.liqmvbanco liq
       JOIn Personal per ON per.PersonalId = liq.persona_id
       LEFT JOIN PersonalCUITCUIL cuit ON cuit.PersonalId = per.PersonalId AND cuit.PersonalCUITCUILId = ( SELECT MAX(cuitmax.PersonalCUITCUILId) FROM PersonalCUITCUIL cuitmax WHERE cuitmax.PersonalId = per.PersonalId) 
