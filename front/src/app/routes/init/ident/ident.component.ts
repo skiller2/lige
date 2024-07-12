3ded755032b4e0d6cfcb7f94c4cf97215b94b2fd
Alfredo <alfredo@orofino.org>
2024-07-12 13:36:21 -0300

    Better text description

--- front/src/app/routes/init/ident/ident.component.ts
+++ front/src/app/routes/init/ident/ident.component.ts
@@ -5,11 +5,12 @@ import { BarcodeFormat } from '@zxing/library';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { firstValueFrom } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
+import { NzResultModule } from 'ng-zorro-antd/result';
 
 @Component({
   selector: 'app-ident',
   standalone: true,
-  imports: [...SHARED_IMPORTS, CommonModule, ZXingScannerModule],
+  imports: [...SHARED_IMPORTS, CommonModule, ZXingScannerModule,NzResultModule ],
   templateUrl: './ident.component.html',
   styleUrl: './ident.component.less'
 })
@@ -29,15 +30,22 @@ export class IdentComponent {
   }
 
   async scanSuccess(e: string) {
-    this.scannerEnabled.set(false)
-    const res: any = await firstValueFrom(this.apiService.getIdentCode(e,this.encTelNro()));
-
-    this.codGenerado.set(String(res?.data?.codigo))
+    try {
+      this.scannerEnabled.set(false)
+      const res: any = await firstValueFrom(this.apiService.getIdentCode(e,this.encTelNro()));
+      this.codGenerado.set(String(res?.data?.codigo))
+    } catch (error) {
+      this.scannerEnabled.set(true)
+    }
   }
   
   ngOnInit() {
     if (this.encTelNro())
       this.scannerEnabled.set(true)
+
+//    this.codGenerado.set('312312312')
+//    this.scannerEnabled.set(false)
+
   }
 
   camerasFoundHandler(cams: any) {
@@ -48,7 +56,7 @@ export class IdentComponent {
   cambioCam() {
     this.curcam++
     if (this.curcam >= this.cams.length)
-      this.curcam = this.cams.length - 1
+      this.curcam = 0
     console.log('cambio a ',this.cams[this.curcam])
     this.camdevice = this.cams[this.curcam].deviceId
     
