1001ab35bcc81f5b7e858059e43a6b5d1669dcfb
Alfredo <alfredo@orofino.org>
2024-08-13 17:03:28 -0300

    Fix bad field names

--- back/src/controller/custodia.controller.ts
+++ back/src/controller/custodia.controller.ts
@@ -164,7 +164,7 @@ export class CustodiaController extends BaseController {
         const origen = objetivoCustodia.origen
         const fecha_fin = objetivoCustodia.fechaFinal? objetivoCustodia.fechaFinal.slice(0, 16).replace('T', ' ') : null
         const destino = objetivoCustodia.destino? objetivoCustodia.destino : null
-        const cant_modulos = objetivoCustodia.cant_modulos? objetivoCustodia.cantModulos : null
+        const cant_modulos = objetivoCustodia.cantModulos? objetivoCustodia.cantModulos : null
         const importe_modulos = objetivoCustodia.impoModulos? objetivoCustodia.impoModulos : null
         const cant_horas_exced = objetivoCustodia.cantHorasExced? objetivoCustodia.cantHorasExced : null
         const impo_horas_exced = objetivoCustodia.impoHorasExced? objetivoCustodia.impoHorasExced : null
@@ -316,7 +316,7 @@ export class CustodiaController extends BaseController {
         SELECT obj.objetivo_custodia_id id, obj.responsable_id responsableId, TRIM(per.PersonalApellidoNombre) responsable,
         obj.cliente_id clienteId, obj.desc_requirente descRequirente, obj.descripcion, obj.fecha_inicio fechaInicio, obj.origen, 
         obj.fecha_fin fechaFinal, obj.destino, obj.cant_modulos cantModulos, obj.importe_modulos impoModulos, 
-        obj.cant_horas_exced cantHorasExced, obj.impo_horas_exced impoHorasExced, obj.cant_km_exced cantKmExced, 
+        obj.cant_horas_exced cantHorasExced, obj.impo_horas_exced impoHorasExced, obj.cant_km_exced cantKmExced, obj.desc_facturacion,
         obj.impo_km_exced impoKmExced, obj.impo_peaje impoPeaje, obj.impo_facturar facturacion, obj.estado, obj.num_factura numFactura
         FROM lige.dbo.objetivocustodia obj
         INNER JOIN Cliente cli ON cli.ClienteId = obj.cliente_id
