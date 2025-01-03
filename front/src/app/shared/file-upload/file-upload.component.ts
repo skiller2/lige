3d4f21436dd54593b30331a59550e580176c3b14
Alfredo <alfredo@orofino.org>
2025-01-03 01:57:27 -0300

    Fix file viewer download

--- front/src/app/shared/file-upload/file-upload.component.ts
+++ front/src/app/shared/file-upload/file-upload.component.ts
@@ -15,7 +15,7 @@ import { NzImageModule } from 'ng-zorro-antd/image';
 @Component({
   selector: 'app-file-upload',
   standalone: true,
-  imports: [SHARED_IMPORTS,NzUploadModule,CommonModule,NgxExtendedPdfViewerModule,NzImageModule],
+  imports: [SHARED_IMPORTS, NzUploadModule, CommonModule, NgxExtendedPdfViewerModule, NzImageModule],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.less',
   providers: [
@@ -26,16 +26,16 @@ import { NzImageModule } from 'ng-zorro-antd/image';
     },
   ],
 })
-export class FileUploadComponent implements ControlValueAccessor{
-  public src=signal<Blob>(new Blob())
+export class FileUploadComponent implements ControlValueAccessor {
+  public src = signal<Blob>(new Blob())
   private readonly http = inject(HttpClient);
 
   constructor() {
     pdfDefaultOptions.assetsFolder = 'assets/bleeding-edge'
 
-   };
+  };
 
-  uploading$ = new BehaviorSubject({loading:false,event:null});
+  uploading$ = new BehaviorSubject({ loading: false, event: null });
   private apiService = inject(ApiService)
   ArchivosAdd: any[] = []
   // valueFile = input()
@@ -64,10 +64,10 @@ export class FileUploadComponent implements ControlValueAccessor{
     switchMap(() => {
       // console.log('input');
       // console.log(this.keyField(), this.idForSearh(), this.textForSearch(), this.tableForSearch(), this.tableForSearch(), this.dataBaseForSearch());
-      
-      if(this.idForSearh() > 0 && this.textForSearch() != "" && this.tableForSearch() != "" && this.dataBaseForSearch() != ""){
+
+      if (this.idForSearh() > 0 && this.textForSearch() != "" && this.tableForSearch() != "" && this.dataBaseForSearch() != "") {
         this.files.set([])
-        return this.apiService.getArchivosAnteriores(this.idForSearh(),this.textForSearch(),this.keyField(),this.tableForSearch(),this.dataBaseForSearch())
+        return this.apiService.getArchivosAnteriores(this.idForSearh(), this.textForSearch(), this.keyField(), this.tableForSearch(), this.dataBaseForSearch())
       } else {
         return []
       }
@@ -81,20 +81,19 @@ export class FileUploadComponent implements ControlValueAccessor{
     }
   }
 
-  async LoadArchivo(documentId: any, filename:any){
-    
+  async LoadArchivo(documentId: any, filename: any) {
+
     this.modalViewerVisiable.set(false)
-    const res = await firstValueFrom(this.http.post('api/file-upload/downloadFile',
-      { 'documentId': documentId, filename: filename , tableForSearch: this.tableForSearch(), dataBaseForSearch: this.dataBaseForSearch()}, { responseType: 'blob' }
-    ))
+
+    const res = await this.LoadArchivoPreview(documentId, filename)
     this.src.set(res)
     this.FileName.set(filename)
     this.modalViewerVisiable.set(true)
   }
 
-   async LoadArchivoPreview(documentId: any, filename:any){
-    const res =  await firstValueFrom(this.http.post('api/file-upload/downloadFile',
-      { 'documentId': documentId, filename: filename, tableSearch: this.tableForSearch(), dataBase: this.dataBaseForSearch() }, { responseType: 'blob' }
+  async LoadArchivoPreview(documentId: any, filename: any) {
+    const res = await firstValueFrom(this.http.post('api/file-upload/downloadFile',
+      { 'documentId': documentId, filename: filename, tableForSearch: this.tableForSearch(), dataBaseForSearch: this.dataBaseForSearch() }, { responseType: 'blob' }
     ))
     return res
   }
@@ -104,9 +103,9 @@ export class FileUploadComponent implements ControlValueAccessor{
   async uploadChange(event: any) {
     switch (event.type) {
       case 'start':
-        
+
         this.uploading$.next({ loading: true, event })
-    
+
         break;
       case 'progress':
         //debugger
@@ -115,18 +114,18 @@ export class FileUploadComponent implements ControlValueAccessor{
         const Error = event.file.error
         if (Error.error.data?.list) {
         }
-        this.uploading$.next({ loading:false,event })
+        this.uploading$.next({ loading: false, event })
         break;
       case 'success':
 
         const Response = event.file.response
-        var url = await this.triggerFunction(Response.data[0].fieldname,Response.data[0].mimetype)
+        var url = await this.triggerFunction(Response.data[0].fieldname, Response.data[0].mimetype)
         Response.data[0].fileUrl = url
-        this.files.set([ ...this.files(), Response.data[0] ])
+        this.files.set([...this.files(), Response.data[0]])
         this.uploading$.next({ loading: false, event })
-        this.apiService.response(Response) 
+        this.apiService.response(Response)
         console.log("files ", this.files())
-       // this.valueExtendedEmitter
+        // this.valueExtendedEmitter
         this.propagateChange(this.files())
 
         break
@@ -137,33 +136,33 @@ export class FileUploadComponent implements ControlValueAccessor{
   }
 
 
-   async triggerFunction(fieldname: any,mimetype:any) {
+  async triggerFunction(fieldname: any, mimetype: any) {
 
     let src = await this.LoadArchivoPreview(0, `${fieldname}.${mimetype.split("/")[1]}`)
-    this.blobUrl =  await URL.createObjectURL(src)
+    this.blobUrl = await URL.createObjectURL(src)
     return this.blobUrl
-    
+
   }
- 
-  async confirmDeleteArchivo( id: string, tipoDocumentDelete : boolean) {
+
+  async confirmDeleteArchivo(id: string, tipoDocumentDelete: boolean) {
     try {
 
       this.ArchivoIdForDelete = parseInt(id);
-      if( tipoDocumentDelete){
+      if (tipoDocumentDelete) {
 
         const ArchivoFilter = this.files().filter((item) => item.fieldname === this.ArchivoIdForDelete)
         this.files.set(ArchivoFilter)
         this.notification.success('Respuesta', `Archivo borrado con exito `)
 
-      }else{
+      } else {
 
         await firstValueFrom(this.apiService.deleteArchivosLicencias(this.ArchivoIdForDelete))
         this.formChange$.next('');
       }
 
-     
+
     } catch (error) {
-      
+
     }
   }
   ////////
@@ -199,5 +198,5 @@ export class FileUploadComponent implements ControlValueAccessor{
 
 
 
-} 
+}
 
