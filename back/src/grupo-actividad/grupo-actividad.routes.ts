ea8d66370c781c80b3e7fa6e3e61a719696ce458
felipe <felipehernandez2014@gmail.com>
2025-02-05 13:24:51 -0300

    fix update date  grupo actividad responsable

--- back/src/grupo-actividad/grupo-actividad.routes.ts
+++ back/src/grupo-actividad/grupo-actividad.routes.ts
@@ -5,45 +5,45 @@ import { grupoActividadController } from "../controller/controller.module";
 export const grupoActividadRouter = Router();
 
 //GRUPO
-grupoActividadRouter.get("/cols", authMiddleware.verifyToken, (req, res) => {
+grupoActividadRouter.get("/cols", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
     grupoActividadController.getGridColsGrupos(req, res);
 });
 
-grupoActividadRouter.post('/listGrupos', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Administrativo'])],  (req, res, next) => {
+grupoActividadRouter.post('/listGrupos', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])],  (req, res, next) => {
     grupoActividadController.listGrupoActividadGrupos(req, res, next)
 })
 
-grupoActividadRouter.post('/changecellgrupo', authMiddleware.verifyToken, (req, res, next) => {
+grupoActividadRouter.post('/changecellgrupo', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
     grupoActividadController.changecellgrupo(req, res, next)
 })
 
-grupoActividadRouter.get('/inactivo_getOptions', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Administrativo'])], (req, res, next) => {
+grupoActividadRouter.get('/inactivo_getOptions', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
     grupoActividadController.getOptions(req, res)
 });
 
-grupoActividadRouter.delete('/grupo', authMiddleware.verifyToken,  (req, res, next) => {
+grupoActividadRouter.delete('/grupo', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])],  (req, res, next) => {
     grupoActividadController.deleteGrupo(req, res, next)
 })
 
 //RESPONSABLES
 
-grupoActividadRouter.get("/colsresponsables", authMiddleware.verifyToken, (req, res) => {
+grupoActividadRouter.get("/colsresponsables", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])],(req, res) => {
     grupoActividadController.getGridColsResponsables(req, res)
 })
 
-grupoActividadRouter.post('/listResponsables', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Administrativo'])],  (req, res, next) => {
+grupoActividadRouter.post('/listResponsables', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])],  (req, res, next) => {
     grupoActividadController.listGrupoActividadResponsables(req, res, next)
 })
 
-grupoActividadRouter.get('/tipo_getOptions', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Administrativo'])], (req, res, next) => {
+grupoActividadRouter.get('/tipo_getOptions', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
     grupoActividadController.getTipos(req, res)
 })
 
-grupoActividadRouter.post('/changecellresponsable', authMiddleware.verifyToken, (req, res, next) => {
+grupoActividadRouter.post('/changecellresponsable', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])],(req, res, next) => {
     grupoActividadController.changecellResponsable(req, res, next)
 })
 
-grupoActividadRouter.delete('/responsables', authMiddleware.verifyToken,  (req, res, next) => {
+grupoActividadRouter.delete('/responsables', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])],  (req, res, next) => {
     grupoActividadController.deleteResponsables(req, res, next)
 })
 //SUPERVISORES
