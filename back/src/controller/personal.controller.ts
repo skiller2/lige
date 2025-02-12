2554d42b705649d95070aa8974d5255a01372372
Boris-CM <bgcolodro@gmail.com>
2025-02-12 16:30:50 -0300

    Fix Antigüedad

--- back/src/controller/personal.controller.ts
+++ back/src/controller/personal.controller.ts
@@ -284,17 +284,7 @@ export class PersonalController extends BaseController {
         )) AS DomicilioCompleto,
         act.GrupoActividadNumero,
         act.GrupoActividadDetalle,
-        suc.SucursalDescripcion,
-        ISNULL(
-          DATEDIFF(YEAR, per.PersonalFechaIngreso, GETDATE()) 
-          - CASE 
-              WHEN (MONTH(per.PersonalFechaIngreso) > MONTH(GETDATE())) 
-                OR (MONTH(per.PersonalFechaIngreso) = MONTH(GETDATE()) AND DAY(per.PersonalFechaIngreso) > DAY(GETDATE())) 
-              THEN 1 
-              ELSE 0 
-            END,
-          0 
-        ) AS antiguedad
+        suc.SucursalDescripcion
         FROM Personal per
         LEFT JOIN PersonalCUITCUIL cuit ON cuit.PersonalId = per.PersonalId AND cuit.PersonalCUITCUILId = ( SELECT MAX(cuitmax.PersonalCUITCUILId) FROM PersonalCUITCUIL cuitmax WHERE cuitmax.PersonalId = per.PersonalId) 
         LEFT JOIN DocumentoImagenFoto foto ON foto.PersonalId = per.PersonalId AND  foto.DocumentoImagenFotoId = per.PersonalFotoId
@@ -2383,6 +2373,20 @@ cuit.PersonalCUITCUILCUIT,
       PersonalBajaFechaActa = @4, PersonalDestruccionNroActa = @5, PersonalFechaDestruccion = @6
       WHERE PersonalId IN (@0)
       `, [personalId, PersonalNroActa, PersonalFechaActa, PersonalBajaNroActa, PersonalBajaFechaActa, PersonalDestruccionNroActa, PersonalFechaDestruccion])
+  }
+
+  async getLugarHabilitacion(req: any, res: Response, next: NextFunction) {
+    const queryRunner = dataSource.createQueryRunner();
+    try {
+      const options = await queryRunner.query(`
+        SELECT LugarHabilitacionId value, TRIM(LugarHabilitacionDescripcion) label
+        FROM LugarHabilitacion
+        WHERE LugarHabilitacionInactivo IS NULL
+      `)
+      this.jsonRes(options, res);
+    } catch (error) {
+      return next(error)
+    }
   }
 
 }
