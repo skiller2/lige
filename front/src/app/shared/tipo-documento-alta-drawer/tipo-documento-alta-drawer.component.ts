138b73c8a2c2e496ae10612d56182e651ae9280d
Boris-CM <bgcolodro@gmail.com>
2025-02-27 12:59:22 -0300

    Add archivo preview in tipo-documento-alta-drawer

--- front/src/app/shared/tipo-documento-alta-drawer/tipo-documento-alta-drawer.component.ts
+++ front/src/app/shared/tipo-documento-alta-drawer/tipo-documento-alta-drawer.component.ts
@@ -1,6 +1,6 @@
 import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
 import { SHARED_IMPORTS } from '@shared';
-import { Component, ChangeDetectionStrategy, model, input, computed, inject, viewChild, signal, TemplateRef,  } from '@angular/core';
+import { Component, ChangeDetectionStrategy, model, input, computed, inject, viewChild, signal, TemplateRef, effect, Injector } from '@angular/core';
 import { FormBuilder, FormArray } from '@angular/forms';
 import { BehaviorSubject, firstValueFrom, debounceTime,switchMap } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
@@ -12,6 +12,8 @@ import { PersonalSearchComponent } from '../personal-search/personal-search.comp
 import { FileUploadComponent } from "../../shared/file-upload/file-upload.component";
 import { ClienteSearchComponent } from "../cliente-search/cliente-search.component";
 import { ObjetivoSearchComponent } from "../objetivo-search/objetivo-search.component";
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-tipo-documento-alta-drawer',
@@ -19,7 +21,8 @@ import { ObjetivoSearchComponent } from "../objetivo-search/objetivo-search.comp
   styleUrl: './tipo-documento-alta-drawer.component.less',
   standalone: true,
   imports: [SHARED_IMPORTS, ReactiveFormsModule, PersonalSearchComponent,
-    CommonModule, FileUploadComponent, ClienteSearchComponent, ObjetivoSearchComponent],
+    CommonModule, FileUploadComponent, ClienteSearchComponent, ObjetivoSearchComponent,
+    NgxExtendedPdfViewerModule, NzImageModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
@@ -32,6 +35,9 @@ export class TipoDocumentoAltaDrawerComponent {
   placement: NzDrawerPlacement = 'left';
   optionsLabels = signal<any[]>([]);
   label = signal<string>('. . .');
+  fileName = signal("")
+  drawerWidth = signal("600px")
+  public src = signal<Blob>(new Blob())
 
   constructor(
     private searchService: SearchService,
@@ -53,19 +59,44 @@ export class TipoDocumentoAltaDrawerComponent {
     return 0
   }
 
+  archivo_mimetype():string {
+    const value:any = this.formTipoDocumento.get("archivo")?.value
+    if (value && value.length && value[0].mimetype){
+      if (value[0].mimetype.includes('image')) return 'image'
+      else if (value[0].mimetype.includes('pdf')) return 'pdf'
+    }
+    return ''
+  }
+
+  archivo_fileUrl():string {
+    const value:any = this.formTipoDocumento.get("archivo")?.value
+    if (value && value.length && value[0].fileUrl) return value[0].fileUrl
+    return ''
+  }
+
+  archivo_originalname():string {
+    const value:any = this.formTipoDocumento.get("archivo")?.value
+    if (value && value.length && value[0].originalname) return value[0].originalname
+    return ''
+  }
+
   async ngOnInit() {
     let now = new Date()
     this.periodo.set({anio:now.getFullYear(), mes:now.getMonth()+1})
 
     const res = await firstValueFrom(this.searchService.getTiposDocumentoOptions())
     this.optionsLabels.set(res)
+
+    this.formTipoDocumento.get('archivo')?.valueChanges.subscribe((value:any) => {
+      if (value.length) this.drawerWidth.set('1000px')
+      else this.drawerWidth.set('600px')
+    });
   }
 
   async save() {
     this.isLoading.set(true)
     const values = this.formTipoDocumento.value
-    console.log(values);
-    
+    // console.log(values);
     try {
       if (this.doc_id()) {
         await firstValueFrom(this.apiService.updateTipoDocumento(values))
