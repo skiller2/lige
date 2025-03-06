0c260fe05ffdc26c845018d6b0604540d7de81c4
felipe <felipehernandez2014@gmail.com>
2025-03-06 17:42:45 -0300

    fix componente reportes

--- back/src/reportes/reportes.routes.ts
+++ back/src/reportes/reportes.routes.ts
@@ -7,3 +7,8 @@ export const reportesRouter = Router();
 reportesRouter.get('/filterReport/:title',  [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => { 
     reportesController.filterReport(req, res, next) 
 })
+
+reportesRouter.post('/descarga',  [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => { 
+    reportesController.Report(req, res, next) 
+})
+
