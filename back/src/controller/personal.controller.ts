0a07e2fb1558268eef284d65f157cc2e54a22887
Boris-CM <bgcolodro@gmail.com>
2025-03-26 10:31:49 -0300

    #674 Fix filter in tipo-documento

--- back/src/controller/personal.controller.ts
+++ back/src/controller/personal.controller.ts
@@ -568,7 +568,7 @@ cuit.PersonalCUITCUILCUIT,
       PersonalApellidoNombreDNILegajo,
       PersonalCUITCUILUltNro,
       PersonalLeyNro,
-      EstadoCivilId,
+      EstadoCivilId
       )
       VALUES (@0,@1,@2,@3,@4,@5,@5,@6,@6,@7,@8,@9,@9,@10,@11,@12,@13)
       
