933cbfb8e187817e275cba12f9cbe1b4a7044ea9
felipe <felipehernandez2014@gmail.com>
2025-04-03 17:43:46 -0300

    fix refactor cursos-instituciones-estudios

--- front/src/app/shared/cursos-drawer/cursos-drawer.component.ts
+++ front/src/app/shared/cursos-drawer/cursos-drawer.component.ts
@@ -1,15 +1,13 @@
 import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
 import { SHARED_IMPORTS } from '@shared';
-import { Component, ChangeDetectionStrategy, model, input, computed, inject, viewChild, signal, TemplateRef, EventEmitter, output, effect,  } from '@angular/core';
+import { Component, ChangeDetectionStrategy, model, input, inject, signal, output, effect } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { NzUploadFile, NzUploadModule } from 'ng-zorro-antd/upload';
-import { BehaviorSubject, firstValueFrom, debounceTime,switchMap } from 'rxjs';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 import { SearchService } from '../../services/search.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
-
 
 type listOptionsT = {
   filtros: any[],
@@ -25,7 +23,6 @@ export interface Option {
 @Component({
     selector: 'app-cursos-drawer',
     imports: [SHARED_IMPORTS, 
-      NzUploadModule, 
       NzAutocompleteModule,
       CommonModule,
     ],
@@ -34,38 +31,20 @@ export interface Option {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-
-
 export class CursosDrawerComponent {
-
-  ArchivosEstudioAdd: any[] = [];
   tituloDrawer = signal<string>("Nuevo Curso")
-  disabled =  input<boolean>(false)
-  RefreshCurso =  model<boolean>(false)
+  disabled = input<boolean>(false)
+  RefreshCurso = model<boolean>(false)
   private apiService = inject(ApiService)
-
   private notification = inject(NzNotificationService)
-
   CursoHabilitacionSelectedId = input.required<number>()
   CursoHabilitacionIdForEdit = signal(0)
-  
   CentroCapacitacionIdSelected = signal(0)
   CentroCapacitacionSedeIdSelected = signal(0)
-
-  PersonalLicenciaAplicaPeriodoHorasMensuales = signal(null)
-
-  options: any[] = [];
-
-  currentDate = new Date()
-  anio = signal(this.currentDate.getFullYear())
-  mes = signal(this.currentDate.getMonth() + 1)
-  
-  isSaving= model<boolean>(false)
+  isSaving = model<boolean>(false)
   placement: NzDrawerPlacement = 'left';
   visible = model<boolean>(false)
-
   onRefreshCurso = output<void>();
-  uploading$ = new BehaviorSubject({loading:false,event:null});
 
   $optionsCentroCapacitacion = this.searchService.getCentroCapacitacionSearch()
   $optionsCentroCapacitacionSede = this.searchService.getCentroCapacitacionSedeSearch()
@@ -97,104 +76,78 @@ export class CursosDrawerComponent {
     effect(async() => {
       const visible = this.visible()
  
-    if (visible) {
-      if (this.CursoHabilitacionSelectedId() > 0) {
-        let vals = await firstValueFrom(this.apiService.getListCursosHistory( { options: this.listOptions }, this.anio(), this.mes(), this.CursoHabilitacionSelectedId(),""))
-       
+      if (visible) {
+        if (this.CursoHabilitacionSelectedId() > 0) {
+          
+          let vals = await firstValueFrom(this.apiService.getListCursosHistory({ options: this.listOptions }, new Date().getFullYear(), new Date().getMonth() + 1, this.CursoHabilitacionSelectedId(), ""))
+         
           this.CursoHabilitacionIdForEdit.set(vals.list[0].CursoHabilitacionId)
-          vals.CursoHabilitacionCodigo = vals.list[0].CursoHabilitacionCodigo,
-          vals.CursoHabilitacionId = vals.list[0].CursoHabilitacionId,
-          vals.CursoHabilitacionDescripcion = vals.list[0].CursoHabilitacionDescripcion,
-          vals.CursoHabilitacionCantidadHoras = vals.list[0].CursoHabilitacionCantidadHoras,
-          vals.CursoHabilitacionVigencia = Number(vals.list[0].CursoHabilitacionVigencia),
-          vals.ModalidadCursoCodigo = vals.list[0].ModalidadCursoCodigo,
-          vals.CursoHabilitacionInstructor = vals.list[0].CursoHabilitacionInstructor,
-          vals.CentroCapacitacionId = vals.list[0].CentroCapacitacionId,
+          vals.CursoHabilitacionCodigo = vals.list[0].CursoHabilitacionCodigo
+          vals.CursoHabilitacionId = vals.list[0].CursoHabilitacionId
+          vals.CursoHabilitacionDescripcion = vals.list[0].CursoHabilitacionDescripcion
+          vals.CursoHabilitacionCantidadHoras = vals.list[0].CursoHabilitacionCantidadHoras
+          vals.CursoHabilitacionVigencia = Number(vals.list[0].CursoHabilitacionVigencia)
+          vals.ModalidadCursoCodigo = vals.list[0].ModalidadCursoCodigo
+          vals.CursoHabilitacionInstructor = vals.list[0].CursoHabilitacionInstructor
+          vals.CentroCapacitacionId = vals.list[0].CentroCapacitacionId
           vals.CentroCapacitacionSedeId = vals.list[0].CentroCapacitacionSedeId
-
-          //console.log("vals",vals)
           
           this.formCli.patchValue(vals)
           this.formCli.markAsUntouched()
           this.formCli.markAsPristine()
 
-          //this.CentroCapacitacionIdSelected.set(vals.list[0].CentroCapacitacionId)
-          //this.CentroCapacitacionSedeIdSelected.set(vals.list[0].CentroCapacitacionSedeId)
-
-        if (this.disabled()) {
-          this.tituloDrawer.set(' Consultar Estudio ');
-          this.formCli.disable()
+          if (this.disabled()) {
+            this.tituloDrawer.set('Consultar Estudio')
+            this.formCli.disable()
+          } else {
+            this.tituloDrawer.set('Editar Estudio')
+            this.formCli.enable()
+          }
         } else {
-          this.tituloDrawer.set('Editar Estudio');
+          this.formCli.reset()
           this.formCli.enable()
-
+          this.CursoHabilitacionIdForEdit.set(0)
         }
-      }else{
-        this.formCli.reset()
-        this.formCli.enable()
-        this.CursoHabilitacionIdForEdit.set(0);
       }
-    }
-    });
+    })
   }
-
-  async ngOnInit(): Promise<void> {
-
-    this.CursoHabilitacionIdForEdit.set(0)
-    this.CentroCapacitacionIdSelected.set(0)
-    this.CentroCapacitacionSedeIdSelected.set(0)
-  }
-
- 
 
   updateValues() {
-    
     setTimeout(() => {
-
-    this.CentroCapacitacionIdSelected.set(this.formCli.value.CentroCapacitacionId || 0)
-    this.formCli.patchValue({CentroCapacitacionSedeId: 0})
-
+      this.CentroCapacitacionIdSelected.set(this.formCli.value.CentroCapacitacionId || 0)
+      this.formCli.patchValue({CentroCapacitacionSedeId: 0})
     }, 1000)
   }
 
   async save() {
-
     this.isSaving.set(true)
     let vals = this.formCli.value
     try {
-
       vals.CursoHabilitacionIdForEdit = this.CursoHabilitacionSelectedId()
+      const res = await firstValueFrom(this.apiService.setCursos(vals))
 
-      const res =  await firstValueFrom(this.apiService.setCursos(vals))
-
-    if(res.data?.list[0]?.CursoHabilitacionId > 0){
-      this.CursoHabilitacionIdForEdit.set(res.data?.list[0]?.CursoHabilitacionId)
-      this.formCli.patchValue({
+      if(res.data?.list[0]?.CursoHabilitacionId > 0){
+        this.CursoHabilitacionIdForEdit.set(res.data?.list[0]?.CursoHabilitacionId)
+        this.formCli.patchValue({
           CursoHabilitacionId: res.data?.list[0]?.CursoHabilitacionId,
           CursoHabilitacionIdForEdit: res.data?.list[0]?.CursoHabilitacionId
         })
-
-        this.tituloDrawer.set('Editar Estudio');
-
+        this.tituloDrawer.set('Editar Estudio')
       }  
       
       this.formCli.markAsUntouched()
       this.formCli.markAsPristine()
       this.onRefreshCurso.emit()
-      //this.formCli$.next("")
     } catch (error) {
-
+      this.notification.error('Error', 'No se pudo guardar el curso')
     }
     this.isSaving.set(false)
   }
 
   async deleteCurso() {
-     let vals = this.formCli.value
-     
+    let vals = this.formCli.value
     let res = await firstValueFrom(this.apiService.deleteCurso(vals))
     this.visible.set(false)
     this.onRefreshCurso.emit()
   }
-
-
 }
--- front/src/app/shared/instituciones-drawer/instituciones-drawer.component.ts
+++ front/src/app/shared/instituciones-drawer/instituciones-drawer.component.ts
@@ -1,15 +1,14 @@
 import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
 import { SHARED_IMPORTS } from '@shared';
-import { Component, ChangeDetectionStrategy, model, input, computed, inject, viewChild, signal, TemplateRef, EventEmitter, output, effect,  } from '@angular/core';
+import { Component, ChangeDetectionStrategy, model, input, computed, inject, signal, output, effect } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { NzUploadModule } from 'ng-zorro-antd/upload';
-import { BehaviorSubject, firstValueFrom } from 'rxjs';
-import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
 import { SearchService } from '../../services/search.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
-
 
 type listOptionsT = {
   filtros: any[],
@@ -24,50 +23,25 @@ export interface Option {
 
 @Component({
     selector: 'app-instituciones-drawer',
-    imports: [SHARED_IMPORTS, 
-      NzUploadModule, 
-      NzAutocompleteModule,
-      CommonModule
-    ],
+    imports: [SHARED_IMPORTS, CommonModule, NzUploadModule, NzAutocompleteModule],
     templateUrl: './instituciones-drawer.component.html',
     styleUrl: './instituciones-drawer.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-
-
-
 export class InstitucionesDrawerComponent {
-
-  ArchivosEstudioAdd: any[] = [];
   tituloDrawer = signal<string>("Nueva Institución")
-  disabled =  input<boolean>(false)
-  RefreshCurso =  model<boolean>(false)
+  disabled = input<boolean>(false)
+  RefreshCurso = model<boolean>(false)
   private apiService = inject(ApiService)
-
   private notification = inject(NzNotificationService)
-
   CentroCapacitacionId = input.required<number>()
   CentroCapacitacionIdForEdit = signal<number>(0)
   PersonalLicenciaAplicaPeriodoHorasMensuales = signal(null)
-
-  options: any[] = [];
-
-  currentDate = new Date()
-  anio = signal(this.currentDate.getFullYear())
-  mes = signal(this.currentDate.getMonth() + 1)
-  
-  isSaving= model<boolean>(false)
+  isSaving = model<boolean>(false)
   placement: NzDrawerPlacement = 'left';
   visible = model<boolean>(false)
-
   onRefreshInstituciones = output<void>();
   uploading$ = new BehaviorSubject({loading:false,event:null});
-
-  listOptions: listOptionsT = {
-    filtros: [],
-    sort: null,
-    extra: null,
-  }
 
   fb = inject(FormBuilder)
   formCli = this.fb.group({
@@ -77,87 +51,61 @@ export class InstitucionesDrawerComponent {
     CentroCapacitacionInactivo: false
   })
 
-  constructor(
-    private searchService: SearchService
-  ) {
+  constructor(private searchService: SearchService) {
     effect(async() => {
       const visible = this.visible()
- 
-    if (visible) {
-      if (this.CentroCapacitacionId() > 0) {
-
-        let vals = await firstValueFrom(this.apiService.getListInstitucionesHistory( { options: this.listOptions }, this.CentroCapacitacionId()))
-      
-        console.log("vals",vals)
-
-        this.CentroCapacitacionIdForEdit.set(vals.list[0].CursoHabilitacionId)
-        vals.CentroCapacitacionId = vals.list[0].CentroCapacitacionId,
-        vals.CentroCapacitacionCuit = vals.list[0].CentroCapacitacionCuit,
-        vals.CentroCapacitacionRazonSocial = vals.list[0].CentroCapacitacionRazonSocial,
-        vals.CentroCapacitacionInactivo = vals.list[0].CentroCapacitacionInactivo
-      
+      if (visible) {
+        if (this.CentroCapacitacionId() > 0) {
+          let vals = await firstValueFrom(this.apiService.getListInstitucionesHistory({ options: { filtros: [], sort: null, extra: null } }, this.CentroCapacitacionId()))
           
-        this.formCli.patchValue(vals)
-        this.formCli.markAsUntouched()
-        this.formCli.markAsPristine()
+          this.CentroCapacitacionIdForEdit.set(vals.list[0].CursoHabilitacionId)
+          vals.CentroCapacitacionId = vals.list[0].CentroCapacitacionId
+          vals.CentroCapacitacionCuit = vals.list[0].CentroCapacitacionCuit
+          vals.CentroCapacitacionRazonSocial = vals.list[0].CentroCapacitacionRazonSocial
+          vals.CentroCapacitacionInactivo = vals.list[0].CentroCapacitacionInactivo
+          
+          this.formCli.patchValue(vals)
+          this.formCli.markAsUntouched()
+          this.formCli.markAsPristine()
 
-
-        if (this.disabled()) {
-          this.tituloDrawer.set(' Consultar Institución ');
-          this.formCli.disable()
-        } else {
-          this.tituloDrawer.set('Editar Institución');
-          this.formCli.enable()
-
+          if (this.disabled()) {
+            this.tituloDrawer.set('Consultar Institución')
+            this.formCli.disable()
+          } else {
+            this.tituloDrawer.set('Editar Institución')
+            this.formCli.enable()
+          }
         }
       }
-    }
     })
-   }
-
-  async ngOnInit(): Promise<void> {
-
-    this.CentroCapacitacionIdForEdit.set(0)
   }
 
   async save() {
-
     this.isSaving.set(true)
     let vals = this.formCli.value
     try {
-
       vals.CentroCapacitacionId = this.CentroCapacitacionId()
+      const res = await firstValueFrom(this.apiService.setInstituciones(vals))
 
-      const res =  await firstValueFrom(this.apiService.setInstituciones(vals))
-
-      if(res.data?.list[0]?.CentroCapacitacionId > 0){
-        
-          this.CentroCapacitacionIdForEdit.set(res.data?.list[0]?.CentroCapacitacionId)
-
-          this.formCli.patchValue({
-              CentroCapacitacionId: res.data?.list[0]?.CentroCapacitacionId,
-          })
-
-          this.tituloDrawer.set('Editar Institución');
-
+      if(res.data?.list[0]?.CentroCapacitacionId > 0) {
+        this.CentroCapacitacionIdForEdit.set(res.data?.list[0]?.CentroCapacitacionId)
+        this.formCli.patchValue({
+          CentroCapacitacionId: res.data?.list[0]?.CentroCapacitacionId,
+        })
+        this.tituloDrawer.set('Editar Institución')
       }  
       
       this.formCli.markAsUntouched()
       this.formCli.markAsPristine()
       this.onRefreshInstituciones.emit()
     } catch (error) {
-
+      // Handle error if needed
     }
     this.isSaving.set(false)
   }
 
   async deleteInstitucion() {
-     let vals = this.formCli.value
-     
-    //let res = await firstValueFrom(this.apiService.deleteInstitucion(vals))
     this.visible.set(false)
     this.onRefreshInstituciones.emit()
   }
-
-
 }
