b017fc81400f3579263c1855829b042707549173
Ignacio Bottaro <ibottaro99@gmail.com>
2025-04-11 16:19:37 -0300

    Fix query setGrupoActividad

--- back/src/controller/personal.controller.ts
+++ back/src/controller/personal.controller.ts
@@ -2259,7 +2259,7 @@ cuit.PersonalCUITCUILCUIT,
         INSERT INTO GrupoActividadPersonal ( 
         GrupoActividadPersonalId, GrupoActividadId, GrupoActividadPersonalPersonalId, GrupoActividadPersonalDesde,
         GrupoActividadPersonalPuesto, GrupoActividadPersonalUsuarioId, GrupoActividadPersonalDia, GrupoActividadPersonalTiempo
-        ) VALUES(@0, @1, @2, @3, @4, @5, @6, @7, @8)
+        ) VALUES(@0, @1, @2, @3, @4, @5, @6, @7)
         `, [GrupoActividadPersonalId, GrupoActividadId, PersonalId, Desde, ip, usuarioId, now, time]
       )
 
