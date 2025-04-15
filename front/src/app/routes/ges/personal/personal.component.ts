203b5237b139c1f5da70b1a19f820298ed0ced4b
Ignacio Bottaro <ibottaro99@gmail.com>
2025-04-15 15:46:25 -0300

    Change StartFilter in personal

--- front/src/app/routes/ges/personal/personal.component.ts
+++ front/src/app/routes/ges/personal/personal.component.ts
@@ -103,7 +103,7 @@ export class PersonalComponent {
       //     selectActiveRow: true
       // }
       this.startFilters = [
-        {field:'SituacionRevistaId', condition:'AND', operator:'=', value:'2;10;11', forced:false},
+        {field:'SituacionRevistaId', condition:'AND', operator:'=', value:'2;10;11;12;20', forced:false},
       ]
   }
 
