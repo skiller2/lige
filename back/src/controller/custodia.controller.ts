cc8e8de385db9db05a960193447be4053eb67eba
Alfredo <alfredo@orofino.org>
2025-05-21 11:54:11 -0300

    Fix error on categorias.filter

--- back/src/controller/custodia.controller.ts
+++ back/src/controller/custodia.controller.ts
@@ -716,7 +716,7 @@ export class CustodiaController extends BaseController {
         const asistenciaController = new AsistenciaController()
         const categorias = await asistenciaController.getCategoriasPorPersonaQuery(anio, mes, PersonalId, 1, queryRunner);
 
-        const categoria = categorias.filter((cat: any) => cat.CategoriaId == 2) //CUSTODIA
+        const categoria = categorias.filter((cat: any) => cat.TipoAsociadoId == 2) //CUSTODIA
         if (categoria.length == 0)
             errores.push(`${sitrev[0].ApellidoNombre} (${PersonalId}) no tiene categoría de custodia vigente al ${this.dateOutputFormat(fechaDesde)}`)
 
