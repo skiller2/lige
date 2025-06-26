9b0fa8f377b957734382d89862fc4e4d18e48386
Ignacio Bottaro <ibottaro99@gmail.com>
2025-06-26 12:41:04 -0300

    Change hasGroup

--- back/src/administradores/administradores.routes.ts
+++ back/src/administradores/administradores.routes.ts
@@ -6,18 +6,18 @@ import { administradoresController } from "../controller/controller.module";
 
 export const AdministradoresRouter = Router();
 
-AdministradoresRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
+AdministradoresRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gComercial', 'gComercialCon'])], (req, res) => {
   administradoresController.getAdministradoresCols(req, res);
 });
 
-AdministradoresRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+AdministradoresRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gComercial', 'gComercialCon'])], (req, res, next) => {
   administradoresController.listAdministradores(req, res, next);
 });
-AdministradoresRouter.get("/cols-clientes", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
+AdministradoresRouter.get("/cols-clientes", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gComercial', 'gComercialCon'])], (req, res) => {
   administradoresController.getAdministradoresColsClientes(req, res);
 });
 
-AdministradoresRouter.post("/list-clientes", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+AdministradoresRouter.post("/list-clientes", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gComercial', 'gComercialCon'])], (req, res, next) => {
   administradoresController.listAdministradoresClientes(req, res, next);
 });
 
