03578e9601a509a28a611c8d6494412adce75648
Alfredo <alfredo@orofino.org>
2025-08-13 16:54:28 -0300

    Fix empty again

--- back/src/importe-venta-vigilancia/importe-venta-vigilancia.controller.ts
+++ back/src/importe-venta-vigilancia/importe-venta-vigilancia.controller.ts
@@ -479,12 +479,12 @@ export class ImporteVentaVigilanciaController extends BaseController {
       for (const row of sheet1.data) {
 
         const clienteCUIT = row[indexCuitCliente]
-        const clienteId = String(row[indexCodigoObjetivo]).split("/")[0]
-        const ClienteElementoDependienteId = String(row[indexCodigoObjetivo]).split("/")[1]
+        const clienteId = row[indexCodigoObjetivo]?.split("/")[0]
+        const ClienteElementoDependienteId = row[indexCodigoObjetivo]?.split("/")[1]
         const importeHoraA = (Math.round(Number(row[indexImporteHoraA]) * 100) / 100)
         const importeHoraB = (Math.round(Number(row[indexImporteHoraB]) * 100) / 100)
 
-        if (!clienteCUIT &&  !importeHoraA && !importeHoraB)
+        if (!clienteCUIT &&  !importeHoraA && !importeHoraB && !clienteId && !ClienteElementoDependienteId )
           continue
 
         //validar que el clientecuit exista y que el id sea el mismo del excel 
