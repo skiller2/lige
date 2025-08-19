7e0927a5f1a23c3a6fb6e167f0e5ae8c415d2da9
Ignacio Bottaro <ibottaro99@gmail.com>
2025-08-19 10:38:46 -0300

    Change query

--- mess/src/controller/personal.controller.ts
+++ mess/src/controller/personal.controller.ts
@@ -7,14 +7,14 @@ export class PersonalController extends BaseController {
   async getDocsPendDescarga(PersonalId: number) {
     const result = await dbServer.dataSource.query(
       `SELECT doc.personalid PersonalId, 
-        doc.DocumentoId, doc.Documentofecha, doc.DocumentoTipoCodigo, tip.detalle, tip.des_den_documento, doc.DocumentoDenominadorDocumento, pr.anio, pr.mes, doc.DocumentoNombreArchivo,
+        doc.DocumentoId, doc.Documentofecha, doc.DocumentoTipoCodigo, tip.DocumentoTipoDetalle, tip.DocumentoTipoDescripcionDenominadorDocumento, doc.DocumentoDenominadorDocumento, pr.anio, pr.mes, doc.DocumentoNombreArchivo,
         MAX(dl.fecha_descarga) fecha_descarga, IIF(dl.doc_id IS NOT NULL,1,0) AS visto
         FROM Documento doc
-        JOIN DocumentoTipo tip ON tip.doctipo_id = doc.DocumentoTipoCodigo
+        JOIN DocumentoTipo tip ON tip.DocumentoTipoCodigo = doc.DocumentoTipoCodigo
         LEFT JOIN lige.dbo.liqmaperiodo pr ON pr.anio = doc.Documentoanio AND pr.mes = doc.Documentomes
         LEFT JOIN lige.dbo.doc_descaga_log dl ON dl.doc_id=doc.DocumentoId AND dl.personal_id = @0
         WHERE doc.DocumentoIndividuoDescargaBot = 1  AND dl.fecha_descarga IS NULL AND (doc.personalid =0 OR doc.personalid =  @0) AND Documentofecha > '2025-01-01'
-        GROUP BY doc.personalid, doc.DocumentoId, doc.Documentofecha, doc.DocumentoTipoCodigo, tip.detalle, des_den_documento, doc.DocumentoDenominadorDocumento, dl.doc_id, pr.anio, pr.mes, doc.DocumentoNombreArchivo`
+        GROUP BY doc.personalid, doc.DocumentoId, doc.Documentofecha, doc.DocumentoTipoCodigo, tip.DocumentoTipoDetalle, DocumentoTipoDescripcionDenominadorDocumento, doc.DocumentoDenominadorDocumento, dl.doc_id, pr.anio, pr.mes, doc.DocumentoNombreArchivo`
       ,
       [PersonalId]
     )
