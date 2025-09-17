462e970c15fd582d51dc5f3ec58dfd8939b6535e
Ignacio Bottaro <ibottaro99@gmail.com>
2025-09-17 10:52:24 -0300

    Change msg and add validations

--- mess/src/flow/flowRemoveTel.ts
+++ mess/src/flow/flowRemoveTel.ts
@@ -6,14 +6,21 @@ import { stop } from "./flowIdle.ts";
 const delay = chatBotController.getDelay()
 
 const flowRemoveTel = addKeyword(EVENTS.ACTION)
-    .addAction(async (ctx, {flowDynamic, state,gotoFlow }) => {
-        await flowDynamic([{ body: `⏱️ un momento`, delay }])
-        const myState = state.getMyState()
-        const personalId = myState.personalId
-        const telefono = ctx.from
+    .addAction(async (ctx, { flowDynamic, state, gotoFlow }) => {
+        try {
+            await flowDynamic([{ body: `⏱️ Un momento.`, delay }])
+            const myState = state.getMyState()
+            const personalId = myState.personalId
+            const telefono = ctx.from
 
-        const res = await personalController.delTelefonoPersona(telefono)
-        return stop(ctx,gotoFlow, state)
+            const res = await personalController.delTelefonoPersona(telefono)
+            return stop(ctx, gotoFlow, state)
+
+        } catch (error) {
+            console.log(error)
+            await flowDynamic(`Ocurrió un error. Por favor, escriba "hola" para iniciar un nuevo chat.`, { delay: delay })
+            return stop(ctx, gotoFlow, state)
+        }
     })
 
 export default flowRemoveTel
