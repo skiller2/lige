9179a8bd85232483a119b6a5c65dfcbb5c145aa4
Alfredo <alfredo@orofino.org>
2025-11-04 18:37:20 -0300

    Add better output msgs

--- back/src/liquidaciones/compensa-general-a-coordinador/compensa-general-a-coordinador.controller.ts
+++ back/src/liquidaciones/compensa-general-a-coordinador/compensa-general-a-coordinador.controller.ts
@@ -25,8 +25,8 @@ export class CompensaGeneralACordinadorController extends BaseController {
 
       const getRecibosGenerados = await recibosController.getRecibosGenerados(queryRunner, periodo_id)
 
-//      if (getRecibosGenerados[0].ind_recibos_generados == 1)
-//        throw new ClientException(`Los recibos para este periodo ya se generaron`)
+      //      if (getRecibosGenerados[0].ind_recibos_generados == 1)
+      //        throw new ClientException(`Los recibos para este periodo ya se generaron`)
 
 
 
@@ -51,9 +51,11 @@ export class CompensaGeneralACordinadorController extends BaseController {
       HAVING SUM(mov.importe*tip.signo )  < 0
       `, [, anio, mes])
 
+      let movimiento_id = await Utils.getMovimientoId(queryRunner)
+
       for (const row of cuentasGnegativo) {
         const PersonalId = row.PersonalId
-        const totalImporteGeneral = row.totalImporte
+        const totalImporteGeneral = Math.abs(row.totalImporte)
         const ApellidoNombre = row.ApellidoNombre
 
         const saldoCuentaCoordinador = await queryRunner.query(
@@ -62,26 +64,71 @@ export class CompensaGeneralACordinadorController extends BaseController {
         JOIN lige.dbo.liqmaperiodo per ON per.periodo_id = mov.periodo_id
         JOIN lige.dbo.liqcotipomovimiento tip ON tip.tipo_movimiento_id = mov.tipo_movimiento_id
         JOIN Personal pers ON pers.PersonalId = mov.persona_id
-        WHERE per.anio = @1 AND per.mes=@2 AND mov.tipocuenta_id='C' AND pers.PersonalId = @0 AND tip.tipo_movimiento_id NOT IN (11,24) 
+        WHERE per.anio = @1 AND per.mes=@2 AND mov.tipocuenta_id='C' AND pers.PersonalId = @0  -- AND tip.tipo_movimiento_id NOT IN (11,24) 
         GROUP BY per.anio, per.mes, pers.PersonalId, mov.tipocuenta_id
         
         `, [PersonalId, anio, mes])
 
         const totalImporteCoordinador = saldoCuentaCoordinador[0]?.totalImporte || 0
+        if (totalImporteCoordinador < 1)
+          continue
 
-        if (totalImporteCoordinador > 0) {
-          movimientos.push({PersonalId, totalImporteGeneral, totalImporteCoordinador,ApellidoNombre})
+
+        const compensaImporte= totalImporteGeneral > totalImporteCoordinador ? totalImporteCoordinador:totalImporteGeneral
+
+        if (compensaImporte) {
+          //          movimientos.push({ PersonalId, totalImporteGeneral, totalImporteCoordinador, ApellidoNombre, compensaImporte })
+          movimientos.push(`${ApellidoNombre} compensa ${compensaImporte}`)
           cantRegistros++
-        }
 
+          await queryRunner.query(
+            `INSERT INTO lige.dbo.liqmamovimientos (movimiento_id, periodo_id, tipo_movimiento_id, tipocuenta_id, fecha, detalle, objetivo_id, persona_id, importe,
+          aud_usuario_ins, aud_ip_ins, aud_fecha_ins, aud_usuario_mod, aud_ip_mod, aud_fecha_mod)
+            VALUES(@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14)
+                  `,
+            [
+              ++movimiento_id,
+              periodo_id,
+              26, //tipo_movimiento_id
+              'G', //tipocuenta_id
+              fechaActual,
+              '',
+              null,
+              PersonalId,
+              totalImporteGeneral,
+              usuario, ip, fechaActual, usuario, ip, fechaActual,
+            ]
+          );
+
+          await queryRunner.query(
+            `INSERT INTO lige.dbo.liqmamovimientos (movimiento_id, periodo_id, tipo_movimiento_id, tipocuenta_id, fecha, detalle, objetivo_id, persona_id, importe,
+          aud_usuario_ins, aud_ip_ins, aud_fecha_ins, aud_usuario_mod, aud_ip_mod, aud_fecha_mod)
+            VALUES(@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14)
+                  `,
+            [
+              ++movimiento_id,
+              periodo_id,
+              27, //tipo_movimiento_id
+              'C', //tipocuenta_id
+              fechaActual,
+              '',
+              null,
+              PersonalId,
+              totalImporteGeneral,
+              usuario, ip, fechaActual, usuario, ip, fechaActual,
+            ]
+          );
+        }
       }
 
+      console.log('movimientos', movimientos)
+
+
+     // throw new ClientException("DEBUG")
       await queryRunner.commitTransaction();
 
-      console.log('movimientos',movimientos)
 
-
-      this.jsonRes({ list: movimientos }, res, `Se procesaron ${cantRegistros} registros `);
+      this.jsonRes({ list: movimientos }, res, ((cantRegistros>0)? `Se procesaron ${cantRegistros} registros <BR>`+movimientos.join('<BR>'):'No hay saldos para compensar'));
     } catch (error) {
       await this.rollbackTransaction(queryRunner)
       return next(error)
