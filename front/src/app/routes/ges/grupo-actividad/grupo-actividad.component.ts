ddd009cc9e9ed15a45d536064317dfff3a19d582
Ignacio Bottaro <ibottaro99@gmail.com>
2025-11-12 10:27:48 -0300

    add seccion informes en grupoActividad

--- front/src/app/routes/ges/grupo-actividad/grupo-actividad.component.ts
+++ front/src/app/routes/ges/grupo-actividad/grupo-actividad.component.ts
@@ -5,6 +5,8 @@ import { TableGrupoActividadGruposComponent } from '../table-grupo-actividad-gru
 import { TableGrupoActividadResponsablesComponent } from '../table-grupo-actividad-responsables/table-grupo-actividad-responsables.component'
 import { TableGrupoActividadObjetivosComponent } from '../table-grupo-actividad-objetivos/table-grupo-actividad-objetivos.component'
 import { TableGrupoActividadPersonalComponent } from '../table-grupo-actividad-personal/table-grupo-actividad-personal.component'
+import { ReporteComponent } from 'src/app/shared/reporte/reporte.component'
+
 
 
 @Component({
@@ -15,7 +17,8 @@ import { TableGrupoActividadPersonalComponent } from '../table-grupo-actividad-p
         TableGrupoActividadGruposComponent,
         TableGrupoActividadResponsablesComponent,
         TableGrupoActividadObjetivosComponent,
-        TableGrupoActividadPersonalComponent
+        TableGrupoActividadPersonalComponent,
+        ReporteComponent
     ],
     templateUrl: './grupo-actividad.component.html',
     styleUrl: './grupo-actividad.component.less'
