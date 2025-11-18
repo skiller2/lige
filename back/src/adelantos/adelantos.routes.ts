f953f6a743567feae51af1219c85298ac7dd8a12
Ignacio Bottaro <ibottaro99@gmail.com>
2025-11-18 10:24:31 -0300

    Change hasGroup

--- back/src/adelantos/adelantos.routes.ts
+++ back/src/adelantos/adelantos.routes.ts
@@ -7,10 +7,10 @@ export const adelantosRouter = Router();
 adelantosRouter.get('/:PersonalId/:anio/:mes', authMiddleware.verifyToken, (req, res, next) => {
     adelantosController.getByPersonalId(Number(req.params.PersonalId), req.params.anio, req.params.mes, req, res, next)
 })
-adelantosRouter.post('', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones','gSistemas','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
+adelantosRouter.post('', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
     adelantosController.setAdelanto(Number(req.body.anio),Number(req.body.mes), Number(req.body.PersonalId), req.body.monto, req, res, next)
 })
-adelantosRouter.delete('/:PersonalId', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones','gSistemas','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
+adelantosRouter.delete('/:PersonalId', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
     adelantosController.delAdelanto(Number(req.params.PersonalId), 0, req.socket.remoteAddress, res, next)
 })
 
@@ -18,6 +18,6 @@ adelantosRouter.get("/cols", authMiddleware.verifyToken, (req, res) => {
     adelantosController.getAdelantoPersonaCols(req, res);
 });
 
-adelantosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas','gSistemas','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
+adelantosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas','gConsejo','Responsables','Administrativo'])], (req, res, next) => {
     adelantosController.getAdelantoPersonaList(req, res, next);
 });
