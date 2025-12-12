0fe9648d0ad1f026324b5d8b01b3424e6ee1ac60
felipe <felipehernandez2014@gmail.com>
2025-12-12 09:35:26 -0300

    #976 collapsed menu in efecto

--- front/src/app/routes/ges/efecto/efecto.component.ts
+++ front/src/app/routes/ges/efecto/efecto.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SHARED_IMPORTS } from '@shared';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { TablePersonalEfectoComponent } from '../table-personal-efecto/table-personal-efecto.component';
 import { TableObjetivosEfectoComponent } from '../table-objetivos-efecto/table-objetivos-efecto.component';
+import { SettingsService } from '@delon/theme';
 @Component({
   selector: 'app-efecto',
   imports: [
@@ -17,5 +18,8 @@ import { TableObjetivosEfectoComponent } from '../table-objetivos-efecto/table-o
   styleUrl: './efecto.component.less'
 })
 export class EfectoComponent {
-
+  private settingsService = inject(SettingsService)
+  ngOnInit() {
+    this.settingsService.setLayout('collapsed', true)
+}
 }
