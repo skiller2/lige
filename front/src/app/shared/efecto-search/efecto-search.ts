774319cb7c0b00b10ca8fb6922cd527265232cfd
Ignacio Bottaro <ibottaro99@gmail.com>
2025-12-29 11:46:20 -0300

    add efecto search component

---
+++ front/src/app/shared/efecto-search/efecto-search.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-efecto-search',
+  imports: [],
+  templateUrl: './efecto-search.html',
+  styleUrl: './efecto-search.scss',
+})
+export class EfectoSearchComponent {
+
+}
