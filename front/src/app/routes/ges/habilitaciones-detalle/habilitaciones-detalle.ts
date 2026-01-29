561a8f5b8ed9d23ce572be89de782380fc90351d
Ignacio Bottaro <ibottaro99@gmail.com>
2026-01-29 11:26:23 -0300

    change date format

--- front/src/app/routes/ges/habilitaciones-detalle/habilitaciones-detalle.ts
+++ front/src/app/routes/ges/habilitaciones-detalle/habilitaciones-detalle.ts
@@ -193,7 +193,7 @@ export class HabilitacionesDetalleComponent {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
 
-    return `${year}/${month}/${day}`;
+    return `${day}/${month}/${year}`;
   }
 
   async deleteDocumento(docId: number) {
