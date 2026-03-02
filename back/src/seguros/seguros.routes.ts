56d3bf7f8d448196dc9e836a2adc9850a0630712
Ignacio Bottaro <ibottaro99@gmail.com>
2026-03-02 10:01:26 -0300

    change hasGroup

--- back/src/seguros/seguros.routes.ts
+++ back/src/seguros/seguros.routes.ts
@@ -12,15 +12,15 @@ segurosRouter.get('/tipo/options', authMiddleware.verifyToken, (req, res, next) 
 });
 
 
-segurosRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getGridCols(req, res);
 })
 
-segurosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getSegurosList(req, res, next);
 })
 
-segurosRouter.post(`/search`, [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.post(`/search`, [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.search(req, res, next);
 }
 )
@@ -30,43 +30,43 @@ segurosRouter.post('/updateSeguros', [authMiddleware.verifyToken, authMiddleware
 }
 )
 
-segurosRouter.get('/cols-poliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/cols-poliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getGridColsPoliza(req, res);
 })
 
-segurosRouter.post('/list-poliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.post('/list-poliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getListPolizaSeguro(req, res, next);
 })
 
-segurosRouter.get('/searchCompaniaSeguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/searchCompaniaSeguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getCompaniaSeguroSearch(req, res, next);
 })
 
-segurosRouter.get('/searchCompaniaSeguroId/:id', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/searchCompaniaSeguroId/:id', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getCompaniaSeguroId(req, res, next);
 })
 
-segurosRouter.get('/poliza/:PolizaSeguroNroPoliza/:PolizaSeguroNroEndoso/:CompaniaSeguroId/:TipoSeguroCodigo', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/poliza/:PolizaSeguroNroPoliza/:PolizaSeguroNroEndoso/:CompaniaSeguroId/:TipoSeguroCodigo', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getPolizaSeguro(req, res, next);
 })
 
-segurosRouter.post('/setpoliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'Liquidaciones', 'gSistemas'])], (req, res, next) => {
+segurosRouter.post('/setpoliza', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'Liquidaciones'])], (req, res, next) => {
   segurosController.setPolizaSeguro(req, res, next);
 })
 
-segurosRouter.get('/searchTipoSeguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/searchTipoSeguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getTipoSeguroSearch(req, res, next);
 })
 
-segurosRouter.get('/searchTipoSeguroId/:id', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/searchTipoSeguroId/:id', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getTipoSeguroId(req, res, next);
 })
 
-segurosRouter.get('/cols-personal-seguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.get('/cols-personal-seguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getColsPersonalSeguro(req, res, next);
 })
 
-segurosRouter.post('/list-personal-seguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas', 'gSistemas'])], (req, res, next) => {
+segurosRouter.post('/list-personal-seguro', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gPersonal', 'gPersonalCon', 'Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   segurosController.getListPersonalSeguro(req, res, next);
 })
 
