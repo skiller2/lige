e4b9120db0aad2fe238f87fe4d74a35f8a66d019
Alfredo <alfredo@orofino.org>
2026-03-03 12:09:09 -0300

    testing code returning fieldErrors

--- front/src/app/routes/ges/parametro-venta-form/parametro-venta-form.ts
+++ front/src/app/routes/ges/parametro-venta-form/parametro-venta-form.ts
@@ -8,7 +8,7 @@ import { firstValueFrom } from 'rxjs';
 import { SearchService } from 'src/app/services/search.service';
 import { ApiService } from 'src/app/services/api.service';
 import { LoadingService } from '@delon/abc/loading';
-import { applyEach, disabled, form, FormField, required, submit, type ValidationError} from '@angular/forms/signals';
+import { applyEach, disabled, FieldTree, form, FormField, required, submit, type ValidationError } from '@angular/forms/signals';
 
 
 export interface Producto {
@@ -307,7 +307,7 @@ export class ParametroVentaFormComponent implements OnInit {
   }
 
   async save() {
-    await submit(this.formParametroVenta, async (form) => {
+    await submit(this.formParametroVenta, async (form:FieldTree<ParametroVentaForm>) => {
       try {
         const formValue = form().value();
 
@@ -322,14 +322,51 @@ export class ParametroVentaFormComponent implements OnInit {
         this.refreshCondVenta.update(v => v + 1)
 
       } catch (e: any) {
-        console.error('Error al guardar condición de venta:', e);
+        console.error('Error al guardar condición de venta:', e.error.data.fieldErrors);
+/*
+        const errors: ValidationError.WithOptionalField[] = [];
+        const key:string='hola'
+        errors.push({
+          fieldTree: this.formParametroVenta().get('PeriodoDesdeAplica'),
+//          fieldTree: form.get('PeriodoDesdeAplica'),
+          kind: 'server',
+          message: 'Test uno dos tre'
+        });
 
+
+
+        const fieldErrors = e.error.data.fieldErrors.map((fe: any) => {
+          const path = fe.fieldTree.replace(/\[(\d+)\]/g, '.$1'); // normalización opcional
+          //const ft = this.formParametroVenta[fe.fieldTree]
+          const ft = this.formParametroVenta().get(fe.fieldTree);
+          return {
+            path,
+            fieldTree: ft,
+
+            kind: fe.kind,
+            message: fe.message,
+          };
+        });
+
+
+        return fieldErrors;
+
+
+        errors.push({
+          //fieldTree: form.PeriodoDesdeAplica,
+          fieldTree: form['PeriodoDesdeAplica'],
+          kind: 'server',
+          message: 'Name is not valid'
+        });
+        console.log('errores', errors)
+        return errors;
         if (e.error.data.fieldErrors) {
 
           // Return the errors to the submit function, which applies them to the form
           return e.error.data.fieldErrors;
 
         }
+        */
       }
       return undefined
     })
