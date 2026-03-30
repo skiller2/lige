baea4e11b1f6a683ef4d763fc21823c062b406e5
Ignacio Bottaro <ibottaro99@gmail.com>
2026-03-30 15:06:52 -0300

    change hasGroup

--- back/src/precios-productos/precios-productos.routes.ts
+++ back/src/precios-productos/precios-productos.routes.ts
@@ -4,38 +4,38 @@ import { preciosProductosController } from "../controller/controller.module.ts";
 
 export const preciosProductosRouter = Router();
 
-preciosProductosRouter.get("/cols-precios", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
+preciosProductosRouter.get("/cols-precios", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res) => {
     preciosProductosController.getGridCols(req, res);
 });
 
-preciosProductosRouter.get("/colsHistory", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
+preciosProductosRouter.get("/colsHistory", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res) => {
   preciosProductosController.colsHistory(req, res);
 });
 
-preciosProductosRouter.get("/options", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.get("/options", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res, next) => {
   preciosProductosController.getProductos(req, res, next);;
 });
 
-preciosProductosRouter.get("/:codigoHistory", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.get("/:codigoHistory", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res, next) => {
   preciosProductosController.listCodigoHistory(req.params.codigoHistory, res, next);;
 });
 
-preciosProductosRouter.post('/list-precios', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.post('/list-precios', [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res, next) => {
     preciosProductosController.listPrecios(req, res, next)
 })
 
-preciosProductosRouter.post('/changecell', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.post('/changecell', [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones', 'gComercial'])], (req, res, next) => {
   preciosProductosController.changecell(req, res, next)
 })
 
-preciosProductosRouter.post('/delete', [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])],  (req, res, next) => {
+preciosProductosRouter.post('/delete', [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','gComercial'])],  (req, res, next) => {
   preciosProductosController.deleteProductos(req, res, next)
 })
 
-preciosProductosRouter.get("/importaciones_anteriores/:anio/:mes", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.get("/importaciones_anteriores/:anio/:mes", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones','Liquidaciones Consultas', 'gComercial', 'gComercialCon'])], (req, res, next) => {
     preciosProductosController.getImportacionesPreciosAnteriores(req, res, next);
 });
 
-preciosProductosRouter.post("/import-xls-precios", [authMiddleware.verifyToken,authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+preciosProductosRouter.post("/import-xls-precios", [authMiddleware.verifyToken,authMiddleware.hasGroup(['Liquidaciones', 'gComercial'])], (req, res, next) => {
     preciosProductosController.handleXLSUpload(req, res, next);
 });
