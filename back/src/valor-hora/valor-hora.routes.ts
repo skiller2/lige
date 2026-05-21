eb797efe9a7bc8ea07ccccb002f5b17b6f7c8d6a
Ignacio Bottaro <ibottaro99@gmail.com>
2026-05-21 10:08:50 -0300

    Update: change hasGroup in valor hora

--- back/src/valor-hora/valor-hora.routes.ts
+++ back/src/valor-hora/valor-hora.routes.ts
@@ -4,26 +4,26 @@ import { valorHoraController } from "../controller/controller.module.ts";
 
 export const valorHoraRouter = Router();
 
-valorHoraRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
+valorHoraRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo', 'gConsejoCon'])], (req, res) => {
     valorHoraController.getValorHoraCols(req, res);
 });
 
-valorHoraRouter.post("/data", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+valorHoraRouter.post("/data", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo', 'gConsejoCon'])], (req, res, next) => {
     valorHoraController.getValorHoraData(req, res, next);
 });
 
-valorHoraRouter.get("/categorias-personal", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+valorHoraRouter.get("/categorias-personal", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo', 'gConsejoCon'])], (req, res, next) => {
     valorHoraController.getCategoriasPersonal(req, res, next);
 });
 
-valorHoraRouter.post("/changecellvalorHora", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+valorHoraRouter.post("/changecellvalorHora", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo'])], (req, res, next) => {
     valorHoraController.changecellvalorHora(req, res, next);
 });
 
-valorHoraRouter.post("/delete", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+valorHoraRouter.post("/delete", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo'])], (req, res, next) => {
     valorHoraController.deleteValorHora(req, res, next);
 });
 
-valorHoraRouter.post("/aumentar", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+valorHoraRouter.post("/aumentar", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gConsejo'])], (req, res, next) => {
     valorHoraController.aumentarValores(req, res, next);
 });
