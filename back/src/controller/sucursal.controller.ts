99fe787c42ea1cc11a99d6e43ae6a5a291d39b14
Ignacio Bottaro <ibottaro99@gmail.com>
2026-05-26 16:10:29 -0300

    Refactor: change query logic

--- back/src/controller/sucursal.controller.ts
+++ back/src/controller/sucursal.controller.ts
@@ -2,20 +2,20 @@ import { BaseController } from "./base.controller.ts";
 import { getConnection } from "../data-source.ts";
 import type { NextFunction, Response } from "express";
 import type { Request } from "express";
+import type { QueryRunner } from "typeorm";
 
 export class SucursalController extends BaseController {
+  static async listSucursalesQuery(queryRunner: QueryRunner) {
+    return await queryRunner.query(
+      'SELECT SucursalId, SucursalDescripcion, SucursalId as value, SucursalDescripcion as label FROM Sucursal '
+    )
+  }
 
   async getAllSucursales(res: Response, req: Request, next: NextFunction) {
     const queryRunner = await getConnection(res.locals.userName);
 
     try {
-      const result = await queryRunner.query(
-        'SELECT SucursalId, SucursalDescripcion, SucursalId as value, SucursalDescripcion as label FROM Sucursal '
-      )
-
-
-
-
+      const result = await SucursalController.listSucursalesQuery(queryRunner)
       this.jsonRes(result, res)
     }
     catch (error) {
