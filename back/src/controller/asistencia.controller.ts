58d78c068c8aa8eadc60518bfa025f35c8de326c
skiller2 <alfgredo@orofino.org>
2026-06-10 18:16:12 -0300

    Fix query error

--- back/src/controller/asistencia.controller.ts
+++ back/src/controller/asistencia.controller.ts
@@ -3973,8 +3973,10 @@ export class AsistenciaController extends BaseController {
   return await queryRunner.query(`
     SELECT TOP 1 a.ActaId, b.ActaFechaActa, b.ActaDescripcion, a.TipoPersonalActaCodigo 
       FROM PersonalActa a 
-      JOIN Acta b ON b.ActaId=a.ActaId AND 
-      WHERE a.PersonalId=@0 AND b.ActaFechaActa<=EOMONTH(DATEFROMPARTS(@1,@2,1))  ORDER BY b.ActaFechaActa DESC `, [personalId, anio, mes])
+      JOIN Acta b ON b.ActaId=a.ActaId
+      WHERE a.PersonalId=@0 AND b.ActaFechaActa<=EOMONTH(DATEFROMPARTS(@1,@2,1))  
+      ORDER BY b.ActaFechaActa DESC `, 
+    [personalId, anio, mes])
 
 }
 
