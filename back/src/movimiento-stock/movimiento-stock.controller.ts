192cc4d22b6130c52be2fd636e1fa62b66502094
skiller2 <alfgredo@orofino.org>
2026-07-16 16:51:47 -0300

    Fix config comprobante

--- back/src/movimiento-stock/movimiento-stock.controller.ts
+++ back/src/movimiento-stock/movimiento-stock.controller.ts
@@ -913,11 +913,11 @@ export class MovimientoStockController extends BaseController {
   async getComprobanteHtmlContentGeneral(fecha: Date, header: string, body: string, footer: string, raw: boolean = false, prev: boolean = false) {
     const imgBuffer = readFileSync(`./assets/logo-lince-full.svg`);
     const imgBase64 = imgBuffer.toString('base64');
-    /*
-        header = (header) ? header : (existsSync(this.PathComprobanteTemplate.header) ? readFileSync(this.PathComprobanteTemplate.header + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.headerDef, 'utf-8'));
-        body = (body) ? body : (existsSync(this.PathComprobanteTemplate.body) ? readFileSync(this.PathComprobanteTemplate.body + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.bodyDef, 'utf-8'));
-        footer = (footer) ? footer : (existsSync(this.PathComprobanteTemplate.footer) ? readFileSync(this.PathComprobanteTemplate.footer + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.footerDef, 'utf-8'));
-    */
+
+    header = (header) ? header : (existsSync(this.PathComprobanteTemplate.header) ? readFileSync(this.PathComprobanteTemplate.header + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.headerDef, 'utf-8'));
+    body = (body) ? body : (existsSync(this.PathComprobanteTemplate.body) ? readFileSync(this.PathComprobanteTemplate.body + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.bodyDef, 'utf-8'));
+    footer = (footer) ? footer : (existsSync(this.PathComprobanteTemplate.footer) ? readFileSync(this.PathComprobanteTemplate.footer + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.footerDef, 'utf-8'));
+
     if (!raw) {
       header = header.replace(/\${imgBase64}/g, imgBase64);
       header = header.replace(/\${fechaFormateada}/g, this.dateOutputFormat(fecha));
@@ -988,10 +988,17 @@ export class MovimientoStockController extends BaseController {
     let content = null;
     let vars = null;
     const prev = false
+
+    const htmls= await this.getComprobanteHtmlContentGeneral(new Date(), header, body, footer, false, prev) 
+    header = htmls.header
+    body = htmls.body
+    footer = htmls.footer
+
+    /*
     header = (header) ? header : (existsSync(this.PathComprobanteTemplate.header) ? readFileSync(this.PathComprobanteTemplate.header + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.headerDef, 'utf-8'));
     body = (body) ? body : (existsSync(this.PathComprobanteTemplate.body) ? readFileSync(this.PathComprobanteTemplate.body + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.bodyDef, 'utf-8'));
     footer = (footer) ? footer : (existsSync(this.PathComprobanteTemplate.footer) ? readFileSync(this.PathComprobanteTemplate.footer + ((prev) ? '.old' : ''), 'utf-8') : readFileSync(this.PathComprobanteTemplate.footerDef, 'utf-8'));
-
+    */
 
     if (movimientoCodigo) {
       const cabecera = await this.getMovimientoCabecera(queryRunner, movimientoCodigo)
