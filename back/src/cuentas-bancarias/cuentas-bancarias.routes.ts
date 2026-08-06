980a1259c727cddbab164b3c78c11661e0e84477
Ignacio Bottaro <ibottaro99@gmail.com>
2026-08-06 16:16:09 -0300

    change hasGroup

--- back/src/cuentas-bancarias/cuentas-bancarias.routes.ts
+++ back/src/cuentas-bancarias/cuentas-bancarias.routes.ts
@@ -4,23 +4,23 @@ import { cuentasBancariasController } from "../controller/controller.module.ts";
 
 export const cuentasBancariasRouter = Router();
 
-cuentasBancariasRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+cuentasBancariasRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   cuentasBancariasController.getColumnsGrid(req, res, next);
 });
 
-cuentasBancariasRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+cuentasBancariasRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   cuentasBancariasController.getCuentasBancarias(req, res, next);
 });
 
-cuentasBancariasRouter.get("/pendientes/:BancoId", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+cuentasBancariasRouter.get("/pendientes/:BancoId", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   cuentasBancariasController.getCuentasPendientesCBU(req, res, next);
 });
 
-cuentasBancariasRouter.post("/import-xls", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+cuentasBancariasRouter.post("/import-xls", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones'])], (req, res, next) => {
     cuentasBancariasController.handleXLSUpload(req, res, next);
 });
 
-cuentasBancariasRouter.post("/add", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+cuentasBancariasRouter.post("/add", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones'])], (req, res, next) => {
   cuentasBancariasController.addCuentasPendientes(req, res, next);
 });
 
