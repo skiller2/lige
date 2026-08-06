3c40ef0d1a4dcce11b1d9a5f4ab86fef9c61b5d6
Ignacio Bottaro <ibottaro99@gmail.com>
2026-08-06 16:22:40 -0300

    change of permissions to testing

--- back/src/precio-efectos/precio-efectos.routes.ts
+++ back/src/precio-efectos/precio-efectos.routes.ts
@@ -4,10 +4,10 @@ import { precioEfectosController } from "../controller/controller.module.ts";
 
 export const precioEfectosRouter = Router();
 
-precioEfectosRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gLogistica', 'gLogisticaCon', 'gSistemas'])], (req, res) => {
+precioEfectosRouter.get("/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res) => {
   precioEfectosController.getGridCols(req, res);
 });
 
-precioEfectosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gLogistica', 'gLogisticaCon', 'gSistemas'])], (req, res, next) => {
+precioEfectosRouter.post("/list", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
   precioEfectosController.listPrecioEfectos(req, res, next);
 });
