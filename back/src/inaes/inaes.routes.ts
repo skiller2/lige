eef7267121080ed453aa0f5f0573b53ec1e59e60
Ignacio Bottaro <ibottaro99@gmail.com>
2026-08-19 17:31:15 -0300

    change hasGroup

--- back/src/inaes/inaes.routes.ts
+++ back/src/inaes/inaes.routes.ts
@@ -4,22 +4,22 @@ import { inaesController } from "../controller/controller.module.ts";
 
 export const inaesRouter = Router();
 
-inaesRouter.get("/altas-bajas/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+inaesRouter.get("/altas-bajas/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   inaesController.getColumnsAltaBajasGrid(req, res, next);
 });
 
-inaesRouter.get("/recibos/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], (req, res, next) => {
+inaesRouter.get("/recibos/cols", [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], (req, res, next) => {
   inaesController.getColumnsRecibosGrid(req, res, next);
 });
 
-inaesRouter.post('/altas-bajas/list', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], async (req, res, next) => {
+inaesRouter.post('/altas-bajas/list', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], async (req, res, next) => {
     await inaesController.getAltasBajas(req, res, next)
 })
 
-inaesRouter.post('/recibos/list', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], async (req, res, next) => {
+inaesRouter.post('/recibos/list', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], async (req, res, next) => {
     await inaesController.getRecibos(req, res, next)
 })
 
-inaesRouter.post('/cuits/from-file', [authMiddleware.verifyToken, authMiddleware.hasGroup(['gSistemas'])], async (req, res, next) => {
+inaesRouter.post('/cuits/from-file', [authMiddleware.verifyToken, authMiddleware.hasGroup(['Liquidaciones', 'Liquidaciones Consultas'])], async (req, res, next) => {
     await inaesController.getCUITsByINAESFile(req, res, next)
 })
